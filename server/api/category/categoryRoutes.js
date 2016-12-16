d730a8e0e5503049ab326018c11c67d2b68d560d
nameisyoung <nameisyoung@gmail.com>
2016-12-16 14:35:51 -0800

    Update categoryRoutes.js

--- server/api/category/categoryRoutes.js
+++ server/api/category/categoryRoutes.js
@@ -1,15 +1,48 @@
 var router = require('express').Router();
+var Model = require('./categoryModel');
+
 
 // setup boilerplate route jsut to satisfy a request
 // for building
-
-//route() will allow you to use same path for different HTTP operation.
-//So if you have same URL but with different HTTP OP such as POST,GET etc
-//Then use route() to remove redundant code.
 router.route('/')
   .get(function(req, res){
-    console.log('Hey from category!!');
-    res.send({ok: true});
+    throw new Error();
+      Model.find(function(err, models){
+	  if(err){
+	      res.send(err);
+	  }
+	  res.json(models);
+      });
+   })
+  .post(function(req, res){
+      Model.create({
+	  categories: req.body
+      });
   });
+
+router.route('/:category_id')
+  .get(function(req, res){
+    Model.find(function(err, models){
+	if(err){
+	    res.send(err);
+	}
+	res.json(models.find({post:category_id});
+  })
+  .delete(function(req, res) {
+    Model.find(function(err, models){
+	if(err){
+	    res.send(err);
+	}
+	res.json(models.remove({category:category_id});
+  })
+  .put(function(req, res) {
+    Model.find(function(err, models){
+	if(err){
+	    res.send(err);
+	}
+	res.json(models.findOneAndUpdate({category:category_id}, update, option, callback);)
+  });
+
+module.exports = router;
 
 module.exports = router;
